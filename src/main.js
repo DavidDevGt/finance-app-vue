a5737dd2280313a2ed90f7e20192aad165a3972b
Davidvl <josuedavidvl18@gmail.com>
2023-08-02 00:18:28 -0600

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.config.productionTup = false
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
